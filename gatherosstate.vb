22a35f28c1f1252620199559ac5f01479ea67de2
Tony <laomms@gmail.com>
2019-07-26 19:23:03 +0700

    Update gatherosstate.vb

--- gatherosstate.vb
+++ gatherosstate.vb
@@ -90,7 +90,7 @@ long __cdecl CreateSppNamedParams(struct _GUID const & __ptr64,struct IUnknown *
 .text:000C5986 ; 73:     v38.Data1 = 0;
 .text:000C5986 mov     [ebp+GuidStruct.Data1], eax
 .text:000C5989 ; 75:     v9 = (void *)CreateInstance(*(int *)((char *)&dword_AECDC0 + v13), &v37);
- int __fastcall CreateInstance(int a1, _DWORD *_dword1)
+ int __fastcall CreateInstance(int a1, _DWORD *a2)
  switch ( a1 )
   {
     case 0:
@@ -140,7 +140,7 @@ long __cdecl CreateSppNamedParams(struct _GUID const & __ptr64,struct IUnknown *
       v4 = CHwidDataCollectorBase::CreateInstance<CHwidBluetoothDataCollector<0>>(a1, a2);
       break;
   }
-
+从固定GUID:eb89a21b_1f9c_4093_9a4d_05d4002543f6中取出相应位置
 .text:000C5997 mov     ebx, eax
 .text:000C5999 ; 76:     v39 = v9;
 .text:000C5999 mov     [ebp+hCreateInstances], ebx
@@ -157,7 +157,7 @@ long __cdecl CreateSppNamedParams(struct _GUID const & __ptr64,struct IUnknown *
 .text:000C59B1 push    [ebp+int3]
 .text:000C59B4 call    Insert
  void *__fastcall Insert(int *int1, int int2, int int3, int *guidstuct_data4)
-定义一个空GUID,经过一系列计算构建一个新的GUID,其中用到了
+构建新GUID
         
 text:00B859C6 mov     ecx, dword ptr [ebp+var_20.Data4]
 .text:00B859C9 push    edi
